ce5a06e944bd29739aad423d7c3010852eeb0930
student-230 <student-230@COMP-230-2>
2023-04-17 08:57:05 +0300

    models step 1

---
+++ Diary_Models/Models/Notes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Diary_Models.Models
+{
+    public class Notes
+    {
+        public Guid Id { get; set; }
+        [Required] [StringLength(50)] public string Name { get; set; }
+        [Required] [StringLength(50)] public string Place { get; set; }
+        public DateTime Date { get; set; }
+        public int Time { get; set; }
+        [Required] [StringLength(500)] public string Description { get; set; }
+        public string Status { get; set; }
+        public string Repeat { get; set; }
+        
+        public virtual User User { get; set; }
+    }
+}
---
+++ Diary_Models/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Diary_Models.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        [Required] [StringLength(50)]public string Name { get; set; }
+        [Required] [StringLength(20)] public string Password { get; set; }
+    }
+}
