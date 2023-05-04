8a1fc57c4dd2379dd2f4a9c0e4e01037e5da0665
student-230 <student-230@COMP-230-2>
2023-05-04 10:00:28 +0300

    controllers

---
+++ Diary_Server/Controllers/NotesController.cs
@@ -0,0 +1,78 @@
+﻿using Diary_dblayer;
+using Diary_Server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diary_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private EntityGateway _db = new();
+
+        private Guid Token => Guid.Parse(Request.Headers["Token"] != string.Empty ?
+                                         Request.Headers["Token"]! : Guid.Empty.ToString());
+        /// <summary>
+        /// All 
+        /// </summary>
+
+        public IActionResult GetAll() =>
+            Ok(new
+            {
+                status = "ok",
+                notesController = _db.GetNotes()
+            });
+
+        /// <summary>
+        /// notes title id
+        /// </summary>
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var potentialTitle = _db.GetNotes(x => x.Id == id).FirstOrDefault();
+            if (potentialTitle is not null)
+                return Ok(new
+                {
+                    status = "ok",
+                    notestitles = potentialTitle
+                });
+            else
+                return NotFound(new
+                {
+                    status = "fail",
+                    message = $"There is no notes title with {id} id"
+                });
+        }
+
+        public IActionResult 
+            
+
+
+
+
+
+
+
+        /*public IActionResult PostNotesTitle([FromBody] NotesTitle notesTitle)
+        {
+            if (LocalAuthService.GetInstance().GetUser(Token) == User.)
+                return Unauthorized(new
+                {
+                    status = "fail",
+                    message = "You have no rights for that action."
+                });
+            _db.AddOrUpdate(notesTitle);
+            return Ok(new
+            {
+                status = "ok",
+                id = notesTitle.Id
+            });
+        }*/
+    }
+}
---
+++ Diary_Server/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Diary_dblayer;
+using Diary_Server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diary_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private EntityGateway _db = new();
+
+        private Guid Token => Guid.Parse(Request.Headers["Token"] != string.Empty ?
+                                         Request.Headers["Token"]! : Guid.Empty.ToString());
+        /// <summary>
+        /// All 
+        /// </summary>
+
+        public IActionResult GetAll() =>
+            Ok(new
+            {
+                status = "ok",
+                usersController = _db.GetUsers()
+            });
+
+        /// <summary>
+        /// users title id
+        /// </summary>
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var potentialTitle = _db.GetUsers(x => x.Id == id).FirstOrDefault();
+            if (potentialTitle is not null)
+                return Ok(new
+                {
+                    status = "ok",
+                    notestitles = potentialTitle
+                });
+            else
+                return NotFound(new
+                {
+                    status = "fail",
+                    message = $"There is no users title with {id} id"
+                });
+        }
+
+        public IActionResult PostUsersTitle([FromBody] UserTitle userTitle)
+        {
+            if (LocalAuthService.GetInstance().GetUser(Token) == User.user)
+                return Unauthorized(new
+                {
+                    status = "fail",
+                    message = "You have no rights for that action."
+                });
+            _db.AddOrUpdate(userTitle);
+            return Ok(new
+            {
+                status = "ok",
+                id = userTitle.Id
+            });
+        }
+    }
+}
